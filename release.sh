7806ccef4e3dab292ed26c00449d930a46dd525a
Kelvin Riker <kelvin.nielsen@spirii.dk>
2024-01-30 06:42:34 +0100

    Adding a release script

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set default values
+update_type="patch"
+pre_release=true
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -r)
+            shift
+            update_type="$1"
+            ;;
+        -prod)
+            pre_release=false
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Get the current branch name
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+
+# Create a new release with the generated version
+if [ "$pre_release" = true ]; then
+
+  #Update version based on the specified type
+  version=$(npm version "$update_type")
+  gh release create "$version" --generate-notes -p --target "$current_branch"
+  echo "Pre-release $version created."
+else
+  version=$(jq -r '.version' package.json)
+  echo $version
+  gh release edit v"$version" --prerelease=false --latest
+  echo "Release $version released."
+fi
