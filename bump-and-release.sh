34d538f96b95b9eda4e72645576fbdab79724326
Kelvin Riker <kelvin.nielsen@spirii.dk>
2024-02-05 06:00:52 +0100

    new release

--- bump-and-release.sh
+++ bump-and-release.sh
@@ -6,16 +6,22 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+# Assign arguments to variables
+version_bump=$1
+release_target=$(git rev-parse --abbrev-ref HEAD)
+
 # Bump the version in package.json
-npm version $1 -m "Bump version to %s"
+npm version $version_bump -m "Bump version to %s"
 
 # Commit the changes
 git add package.json
 git commit -m "Bump version"
 
-# Push the changes to the main branch
-git push origin main
+# Push the changes to the current branch
+git push origin $release_target
 
-# Create a GitHub release
-gh release create v$(node -p "require('./package.json').version") -t "Release $(node -p "require('./package.json').version")" -n "Release notes for $(node -p "require('./package.json').version")"
-
+# Create a GitHub release with the specified target
+gh release create v$(node -p "require('./package.json').version") \
+  -t "Release $(node -p "require('./package.json').version")" \
+  -n "Release notes for $(node -p "require('./package.json').version")" \
+  --target $release_target
