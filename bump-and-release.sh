2f6f5d9bf81b8dd09f4a7e5bdbf7aa622e59f688
Kelvin Riker <kelvin.nielsen@spirii.dk>
2024-02-05 06:03:52 +0100

    More releases

--- bump-and-release.sh
+++ bump-and-release.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <major|minor|patch>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+  echo "Usage: $0 <major|minor|patch> [target-branch] [production]"
   exit 1
 fi
 
 # Assign arguments to variables
 version_bump=$1
-release_target=$(git rev-parse --abbrev-ref HEAD)
+release_target=${2:-main}
+is_production=${3:-}
 
 # Bump the version in package.json
 npm version $version_bump -m "Bump version to %s"
@@ -17,11 +18,13 @@ npm version $version_bump -m "Bump version to %s"
 git add package.json
 git commit -m "Bump version"
 
-# Push the changes to the current branch
+# Push the changes to the specified branch
 git push origin $release_target
 
 # Create a GitHub release with the specified target
 gh release create v$(node -p "require('./package.json').version") \
   -t "Release $(node -p "require('./package.json').version")" \
   -n "Release notes for $(node -p "require('./package.json').version")" \
+  --target $release_target \
+  $( [ "$is_production" == "production" ] && echo "--title production --notes \"This is a production release\"" || echo "--prerelease" )
   --target $release_target
